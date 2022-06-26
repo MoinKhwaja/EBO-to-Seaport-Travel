300d2b8ecadb0c8022412ecb1db3ab3206276a9b
Moin <moinkhwaja1997@gmail.com>
2022-06-26 14:30:51 -0400

    added MBTA basic API for alerts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import MBTAAlert from './components/MBTAAlert'
+import { useState, useEffect } from "react";
 
 function App() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.allorigins.win/raw?url=https://cdn.mbta.com/realtime/Alerts.json/')
+    .then(response => response.json())
+    .then(data =>setData(data.entity.filter(obj => 
+      obj.alert.informed_entity.some(ie => ie.route_id === "Blue")
+    )));
+  },[]);
+  
   return (
     <div className="App">
       <h2>hello</h2>
+      {data.map((alert) => (<MBTAAlert alert= {alert}/>))}
     </div>
   );
 }
 
+
 export default App;
+
+//twitter keys 
+//api key: OvacvXaLNTTRJvozdaokdbgpL
+//api secret: 6GtiR48w3py4d1UEaNbZRU9Jj1TZDcolQS2B5vM4GGy2zg3TX0
+//bearer token: AAAAAAAAAAAAAAAAAAAAAA2GcgEAAAAAdkzuO0ZJogtsxeODYPOvtlFb3%2F0%3DOqVwyBpwvhcFNmthZhI2jNyMFD2eEQV0pLaXs9DPk9tKxHsi8t
---
+++ src/components/MBTAAlert.js
@@ -0,0 +1,11 @@
+//Take in a single alert and displays it
+
+function MBTAAlert(props) {
+    return (
+        <div style={{"border-style": "solid"}}>   
+        <p>{props.alert.alert.header_text.translation[0].text}</p>
+        <p>{props.alert.alert.description_text.translation[0].text}</p>
+        </div>) 
+};
+
+export default MBTAAlert;
