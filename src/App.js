91aed16ccdc7a74fd0948fb3b1a505c9a6bedd35
MMLeBlanc <matthewmleblanc@gmail.com>
2022-07-10 19:20:25 -0500

    removed unecessary info from component and calculated arrival time

--- src/App.js
+++ src/App.js
@@ -19,50 +19,11 @@ function App() {
   const [schedule, setSchedule] = useState([]);
 
   useEffect(() => {
-    fetch("https://api-v3.mbta.com/predictions?page%5Blimit%5D=2&sort=-arrival_time&include=stop&filter%5Bdirection_id%5D=1&filter%5Broute_type%5D=3&filter%5Broute%5D=743")
+    fetch("https://api-v3.mbta.com/predictions?page%5Blimit%5D=2&sort=arrival_time&filter%5Bdirection_id%5D=1&filter%5Broute_type%5D=3&filter%5Broute%5D=743&filter%5Bstop%5D=place-aport")
   .then(response => response.json())
   .then(res => setSchedule(res.data))
   .catch(error => console.log('error', error));
   }, [])
-
-  const dummyData = [{
-    "attributes": {
-        "arrival_time": null,
-        "departure_time": null,
-        "direction_id": 1,
-        "schedule_relationship": null,
-        "status": null,
-        "stop_sequence": 5
-    },
-    "id": "prediction-52277330-7097-5",
-    "relationships": {
-        "route": {
-            "data": {
-                "id": "743",
-                "type": "route"
-            }
-        },
-        "stop": {
-            "data": {
-                "id": "7097",
-                "type": "stop"
-            }
-        },
-        "trip": {
-            "data": {
-                "id": "52277330",
-                "type": "trip"
-            }
-        },
-        "vehicle": {
-            "data": {
-                "id": "y1106",
-                "type": "vehicle"
-            }
-        }
-    },
-    "type": "prediction"
-}]
   
   return (
     <div className="App">
